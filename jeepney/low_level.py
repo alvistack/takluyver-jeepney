38641e76c0acc51020715100b7867743c5f30876
Thomas Kluyver <thomas@kluyver.me.uk>
2017-05-27 16:41:32 +0100

    Allow serialising dicts
    Rather than expecting lists of tuples.

--- jeepney/low_level.py
+++ jeepney/low_level.py
@@ -178,6 +178,8 @@ class Array:
         return res, pos
 
     def serialise(self, data, pos, endianness):
+        if isinstance(self.elt_type, DictEntry) and isinstance(data, dict):
+            data = data.items()
         pad1 = padding(pos, self.alignment)
         pos_after_length = pos + pad1 + 4
         pad2 = padding(pos_after_length, self.elt_type.alignment)
