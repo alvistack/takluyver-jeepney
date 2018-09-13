a8097e64f4d5cda2ac3d1d25f84c36a249e62210
Jane Soko <boynamedjane@misled.ml>
2018-09-13 00:41:27 -0700

    Replace Desktop Notifications aio example

--- examples/aio_subscribe.py
+++ examples/aio_subscribe.py
@@ -1,77 +1,53 @@
-# The note at the top of blocking_subscribe.py also applies to this example.
+
+# Note: "subscribe" in the filename refers to the router.subscribe_signal API
+# method and not the process of adding match rules to the message bus.
 
 import asyncio
 
 from jeepney.integrate.asyncio import connect_and_authenticate, Proxy
 from jeepney.bus_messages import message_bus, MatchRule
-from aio_notify import Notifications
 
 
 def print_match(match_info):
-    """A demo callback.
-    Called with a single tuple on successful matches.
-    """
-    reasons = (  # <https://developer.gnome.org/notification-spec>
-        "Expired",
-        "Dismissed by the user",
-        "Closed by a call to CloseNotification",
-        "Undefined/reserved reasons"
-    )
-    idee, reason = match_info
-    print("<- %d %s" % (idee, reasons[reason]))
+    """A demo callback triggered on successful matches."""
+    print("[watcher] match hit:", match_info)
 
 
 async def main():
-    __, proto = await connect_and_authenticate("SESSION")
-    session_bus = Proxy(message_bus, proto)
-    notify_msggen = Notifications()
-    notify_service = Proxy(notify_msggen, proto)
+    __, service_proto = await connect_and_authenticate("SESSION")
+    __, watcher_proto = await connect_and_authenticate("SESSION")
+    service = Proxy(message_bus, service_proto)
+    watcher = Proxy(message_bus, watcher_proto)
 
-    # Create a "signal-selection" match rule used by *Match methods below
+    # Create a "signal-selection" match rule
     match_rule = MatchRule(
         type="signal",
-        sender=notify_msggen.bus_name,
-        interface=notify_msggen.interface,
-        member="NotificationClosed",
-        path=notify_msggen.object_path,
+        sender=message_bus.bus_name,
+        interface=message_bus.interface,
+        member="NameOwnerChanged",
+        path=message_bus.object_path,
     )
 
-    # Prep and register callback
-    proto.router.subscribe_signal(
+    # Arg number 0 must match the string below (try changing either)
+    match_rule.add_arg_condition(0, "org.jeepney.aio_subscribe")
+
+    # Register a callback
+    watcher_proto.router.subscribe_signal(
         callback=print_match,
-        path=notify_msggen.object_path,
-        interface=notify_msggen.interface,
-        member="NotificationClosed"
+        path=message_bus.object_path,
+        interface=message_bus.interface,
+        member="NameOwnerChanged"
     )
 
-    idee, *__ = await notify_service.Notify("aio_subscribe", 0, "",
-                                            "Foo", "foo", [], {}, -1,)
-    print("->", idee, "+")  # ~~> notified
-    await asyncio.sleep(1)
-    print("->", idee, "-")  # ~~> closing
-    await notify_service.CloseNotification(idee)
-
-    print("# Starting subscription")
-    await session_bus.AddMatch(match_rule)
+    print("[watcher] adding match rule")
+    await watcher.AddMatch(match_rule)
     await asyncio.sleep(1)
 
-    idee, *__ = await notify_service.Notify("aio_subscribe", 0, "",
-                                            "Bar", "bar", [], {}, -1,)
-    print("->", idee, "+")
-    await asyncio.sleep(1)
-    print("->", idee, "-")
-    await notify_service.CloseNotification(idee)
+    print("[service] calling 'RequestName'")
+    resp = await service.RequestName("org.jeepney.aio_subscribe", 4)
 
-    print("# Cancelling subscription")
-    await session_bus.RemoveMatch(match_rule)
-    await asyncio.sleep(1)
-
-    idee, *__ = await notify_service.Notify("aio_subscribe", 0, "",
-                                            "Baz", "baz", [], {}, -1,)
-    print("->", idee, "+")
-    await asyncio.sleep(1)
-    print("->", idee, "-")
-    await notify_service.CloseNotification(idee)
+    print("[service] reply:", (None, "primary owner", "in queue",
+                               "exists", "already owned")[resp[0]])
 
 
 if __name__ == "__main__":
