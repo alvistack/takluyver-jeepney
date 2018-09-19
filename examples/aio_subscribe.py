0d9bef74448e48b3a98ccb516b3de83a3351dac3
Jane Soko <boynamedjane@misled.ml>
2018-09-18 19:31:06 -0700

    Add docstring to aio-subscribe example
    - Replace phony domain name with something legitimate
    - Add exit delay

--- examples/aio_subscribe.py
+++ examples/aio_subscribe.py
@@ -1,11 +1,28 @@
+"""
+This example simulates two clients interacting with the message bus, more or
+less independently.
 
-# Note: "subscribe" in the filename refers to the router.subscribe_signal API
-# method and not the process of adding match rules to the message bus.
+Client 1
+    Any app on the bus (here, called *service*). It asks the bus for sole
+    custody of its preferred name, a `well-known bus name`_ that it wants
+    others to recognize.
+
+Client 2
+    An interested party (here, called *watcher*). It asks the bus to emit a
+    signal (send it an update) whenever *service*'s well-known bus name changes
+    hands.  It uses Jeepney's ``router.subscribe_signal`` to fire a callback
+    every time this occurs.
+
+.. _well-known bus name: https://dbus.freedesktop.org/doc
+   /dbus-specification.html#message-protocol-names
+"""
 
 import asyncio
 
 from jeepney.integrate.asyncio import connect_and_authenticate, Proxy
 from jeepney.bus_messages import message_bus, MatchRule
+
+well_known_bus_name = "io.readthedocs.jeepney.aio_subscribe_example"
 
 
 def print_match(match_info):
@@ -16,6 +33,7 @@ def print_match(match_info):
 async def main():
     __, service_proto = await connect_and_authenticate("SESSION")
     __, watcher_proto = await connect_and_authenticate("SESSION")
+    # Interface objects used by each app to interact with the message bus
     service = Proxy(message_bus, service_proto)
     watcher = Proxy(message_bus, watcher_proto)
 
@@ -28,8 +46,8 @@ async def main():
         path=message_bus.object_path,
     )
 
-    # Arg number 0 must match the string below (try changing either)
-    match_rule.add_arg_condition(0, "org.jeepney.aio_subscribe")
+    # Condition: arg number 0 must match the bus name (try changing either)
+    match_rule.add_arg_condition(0, well_known_bus_name)
 
     # Register a callback
     watcher_proto.router.subscribe_signal(
@@ -44,11 +62,12 @@ async def main():
     await asyncio.sleep(1)
 
     print("[service] calling 'RequestName'")
-    resp = await service.RequestName("org.jeepney.aio_subscribe", 4)
+    resp = await service.RequestName(well_known_bus_name, 4)
 
     print("[service] reply:", (None, "primary owner", "in queue",
                                "exists", "already owned")[resp[0]])
 
+    await asyncio.sleep(1)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
