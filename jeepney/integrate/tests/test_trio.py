91d048f2f8d9e3d76f1e9b94a5f3e014e18210e2
Thomas Kluyver <thomas@kluyver.me.uk>
2020-06-13 18:12:39 +0100

    Mark whole test modules as using trio/asyncio

--- jeepney/integrate/tests/test_trio.py
+++ jeepney/integrate/tests/test_trio.py
@@ -3,11 +3,13 @@ import pytest
 from jeepney.integrate.trio import open_dbus_connection
 from .utils import have_session_bus
 
-pytestmark = pytest.mark.skipif(
-    not have_session_bus, reason="Tests require DBus session bus"
-)
+pytestmark = [
+    pytest.mark.trio,
+    pytest.mark.skipif(
+        not have_session_bus, reason="Tests require DBus session bus"
+    ),
+]
 
-@pytest.mark.trio
 async def test_connect():
     conn = await open_dbus_connection(bus='SESSION')
     async with conn:
