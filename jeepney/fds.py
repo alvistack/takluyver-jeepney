59e76fc408683d02ee38a9bcc7c5365a77a3e681
Thomas Kluyver <thomas@kluyver.me.uk>
2021-04-12 09:55:23 +0100

    Object to represent received file descriptors

---
+++ jeepney/fds.py
@@ -0,0 +1,103 @@
+import array
+import os
+import socket
+from warnings import warn
+
+class WrappedFD:
+    """A file descriptor received in a D-Bus message
+
+    This wrapper helps ensure that the file descriptor is closed exactly once.
+    You can convert it into a file or socket object.
+    """
+    __slots__ = ('_fd',)
+    _CLOSED = -1
+    _CONVERTED = -2
+
+    def __init__(self, fd):
+        self._fd = fd
+
+    def __repr__(self):
+        detail = self._fd
+        if self._fd == self._CLOSED:
+            detail = 'closed'
+        elif self._fd == self._CONVERTED:
+            detail = 'converted'
+        return f"<WrappedFD ({detail})>"
+
+    def close(self):
+        """Close the file descriptor
+
+        This can safely be called multiple times, but will raise RuntimeError
+        if called after converting it with one of the ``to_*`` methods.
+
+        This object can also be used in a ``with`` block, and then leaving the
+        block closes it.
+        """
+        if self._fd == self._CLOSED:
+            pass
+        elif self._fd == self._CONVERTED:
+            raise RuntimeError("Can't close WrappedFD after converting it")
+        else:
+            self._fd, fd = self._CLOSED, self._fd
+            os.close(fd)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def __del__(self):
+        if self._fd >= 0:
+            warn(
+                f'WrappedFD ({self._fd}) was neither closed nor converted',
+                ResourceWarning, stacklevel=2, source=self
+            )
+            self.close()
+
+    def fileno(self):
+        """Get the integer file descriptor, without affecting the wrapper"""
+        return self._fd
+
+    def to_raw_fd(self):
+        """Convert to the low-level integer file descriptor
+
+        The wrapper object can't be used after calling this. The caller is
+        responsible for closing the file descriptor.
+        """
+        self._fd, fd = self._CONVERTED, self._fd
+        return fd
+
+    def to_file(self, mode, encoding=None, errors=None):
+        """Convert to a Python file object
+
+        The arguments are as passed to open().
+
+        The wrapper object can't be used after calling this. Closing the file
+        object will also close the file descriptor.
+        """
+        return open(self.to_raw_fd(), mode, encoding=encoding, errors=errors)
+
+    def to_socket(self):
+        """Convert to a socket object
+
+        This returns a standard library :class:`socket.socket` object.
+
+        The wrapper object can't be used after calling this. Closing the socket
+        object will also close the file descriptor.
+        """
+        from socket import socket
+        return socket(fileno=self.to_raw_fd())
+
+    @classmethod
+    def from_ancdata(cls, ancdata) -> ['WrappedFD']:
+        """Make a list of WrappedFD from received file descriptors
+
+        ancdata is a list of ancillary data tuples as returned by socket.recvmsg()
+        """
+        fds = array.array("i")  # Array of ints
+        for cmsg_level, cmsg_type, data in ancdata:
+            if cmsg_level == socket.SOL_SOCKET and cmsg_type == socket.SCM_RIGHTS:
+                # Append data, ignoring any truncated integers at the end.
+                fds.frombytes(data[:len(data) - (len(data) % fds.itemsize)])
+        return [cls(i) for i in fds]
