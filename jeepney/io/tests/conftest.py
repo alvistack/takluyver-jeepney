6667e6560c581cfb19ca4dc04287e51c0999016b
Thomas Kluyver <thomas@kluyver.me.uk>
2021-07-03 18:56:55 +0100

    Add integration tests for sending FDs

--- jeepney/io/tests/conftest.py
+++ jeepney/io/tests/conftest.py
@@ -40,5 +40,42 @@ def respond_with_fd():
     reply_thread.join()
 
 
+@pytest.fixture()
+def read_from_fd():
+    name = "io.gitlab.takluyver.jeepney.tests.read_from_fd"
+    addr = DBusAddress(bus_name=name, object_path='/')
 
+    with open_dbus_connection(bus='SESSION', enable_fds=True) as conn:
+        with DBusRouter(conn) as router:
+            status, = Proxy(message_bus, router).RequestName(name)
+        assert status == 1  # DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER
+
+        def _reply_once():
+            while True:
+                msg = conn.receive()
+                if msg.header.message_type is MessageType.method_call:
+                    if msg.header.fields[HeaderFields.member] == 'ReadFD':
+                        with msg.body[0].to_file('rb') as f:
+                            f.seek(0)
+                            s = f.read().decode('utf-8')
+                        conn.send(new_method_return(msg, 's', (s,)))
+                        return
+                    else:
+                        conn.send(new_error(msg, 'NoMethod'))
+
+        reply_thread = threading.Thread(target=_reply_once, daemon=True)
+        reply_thread.start()
+        yield addr
+
+    reply_thread.join()
+
+
+@pytest.fixture()
+def temp_file_and_contents():
+    data = b'abc123'
+    with TemporaryFile('w+b') as tf:
+        tf.write(data)
+        tf.flush()
+        tf.seek(0)
+        yield tf, data
 
--- jeepney/io/tests/test_blocking.py
+++ jeepney/io/tests/test_blocking.py
@@ -76,3 +76,13 @@ def test_recv_fd(respond_with_fd):
     assert reply.header.message_type is MessageType.method_return
     with reply.body[0].to_file('w+') as f:
         assert f.read() == 'readme'
+
+
+def test_send_fd(temp_file_and_contents, read_from_fd):
+    temp_file, data = temp_file_and_contents
+    readfd_call = new_method_call(read_from_fd, 'ReadFD', 'h', (temp_file,))
+    with open_dbus_connection(bus='SESSION', enable_fds=True) as conn:
+        reply = conn.send_and_get_reply(readfd_call, timeout=5)
+
+    assert reply.header.message_type is MessageType.method_return
+    assert reply.body[0] == data.decode('utf-8')
