68e057d059857ce46f3aefb4bbf9f4ceadcfea21
Damjan Jovanovic <damjan.jov@gmail.com>
2023-12-23 19:34:37 +0200

    sizeof(struct cmsgcred) == 84 on FreeBSD 13.2 on AMD64, but pass a 512 byte array just in case.

--- jeepney/io/blocking.py
+++ jeepney/io/blocking.py
@@ -310,7 +310,7 @@ def prep_socket(addr, enable_fds=False, timeout=2.0) -> socket.socket:
         with_sock_deadline(sock.connect, addr)
         authr = Authenticator(enable_fds=enable_fds)
         if hasattr(socket, 'SCM_CREDS'):
-            sock.sendmsg([b'\0'], [(socket.SOL_SOCKET, socket.SCM_CREDS, b'')])
+            sock.sendmsg([b'\0'], [(socket.SOL_SOCKET, socket.SCM_CREDS, bytes(512))])
         else:
             sock.send(b'\0')
         for req_data in authr:
